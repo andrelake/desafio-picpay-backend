8241c04a007b59e715de4bfd4d61faec4cab33f1
andreLake <andre.lake@hotmail.com>
2024-04-08 09:48:23 -0300

    project created

---
+++ src/test/java/com/andrelake/desafiopicpaybackend/DesafioPicpayBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrelake.desafiopicpaybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioPicpayBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
